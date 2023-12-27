4c34b2ef286823d34f8f4c260ba6d3b473f91939
Ruth581 <151011715+kolobovrodion2@users.noreply.github.com>
2023-12-28 02:10:51 +0700

    Create super-guacamole.swift

---
+++ super-guacamole.swift
@@ -0,0 +1,10 @@
+    }
+}
+
+$userDB = new UserDatabase();
+
+// Регистрация нового пользователя
+$userDB->addUser("alice", "password123");
+
+// Попытка входа пользователя
+$userDB->login("alice", "password123");
